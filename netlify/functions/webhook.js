920af8722c0826cfe5bab7d87a9c6264a1c7db6e
yungsuKo <yungsu2391@gmail.com>
2025-05-08 16:24:51 +0900

    웹훅 zendesk channel alim 추가

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -3,12 +3,36 @@ const axios = require('axios'); // axios로 HTTP 요청 보낼 거야.
 exports.handler = async (event, context) => {
   console.log('Webhook received:', event.body);
 
-  const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL_TOM;
+  const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL_ZENDESK;
 
   // 보낼 메시지 구성
-  const message = {
-    text: `새로운 웹훅 수신! 데이터: ${event.body}`,
-  };
+  const message = {};
+  if (event.body.event_no == 90027) {
+    let info_msg = event.body.extra_info.map(
+      (info) => `    - ${info.ord_item_code} (${info.supplier_code}) \n`
+    );
+    message.text = `반품이 신청 되었어요.\n
+    \n
+    - 주문번호 : ${event.body.order_id} \n
+    - 주문일자 : ${event.body.payment_date.slice(0, 10)} \n
+    - 반품 품목
+    ${info_msg}
+    - 반품사유 : ${event.body.claim_reason} \n
+    
+    `;
+  } else if (event.body.event_no == 90028) {
+    let info_msg = event.body.extra_info.map(
+      (info) => `    - ${info.ord_item_code} (${info.supplier_code}) \n`
+    );
+    message.text = `교환이 신청 되었어요.\n
+    \n
+    - 주문번호 : ${event.body.order_id} \n
+    - 주문일자 : ${event.body.payment_date.slice(0, 10)} \n
+    - 교환 품목
+    ${info_msg}
+    - 교환사유 : ${event.body.claim_reason} \n
+    `;
+  }
 
   try {
     // Slack에 POST 요청
