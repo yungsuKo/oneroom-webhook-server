d1783fa1669cfbeccba7069c8670cbeb0f43be5c
yungsuKo <yungsu2391@gmail.com>
2025-06-10 13:41:26 +0900

    전체 try catch

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -1,69 +1,75 @@
 const axios = require('axios'); // axios로 HTTP 요청 보낼 거야.
 const crypto = require('crypto');
 const { getCache, setCache } = require('./utils/cacheUtil');
+const supplierMap = require('./supplier_name.json');
 
 exports.handler = async (event, context) => {
-  console.log('Webhook received:', event.body);
-  const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL_ZENDESK;
-  const zendeskDomain = process.env.ZENDESK_DOMAIN;
-  const zendeskEmail = process.env.ZENDESK_EMAIL;
-  const zendeskToken = process.env.ZENDESK_API_TOKEN;
+  try {
+    console.log('Webhook received:', event.body);
+    const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL_ZENDESK;
+    const zendeskDomain = process.env.ZENDESK_DOMAIN;
+    const zendeskEmail = process.env.ZENDESK_EMAIL;
+    const zendeskToken = process.env.ZENDESK_API_TOKEN;
 
-  const body = JSON.parse(event.body);
-  const today = new Date();
-  const formattedDate =
-    today.getFullYear() +
-    '-' +
-    String(today.getMonth() + 1).padStart(2, '0') +
-    '-' +
-    String(today.getDate()).padStart(2, '0');
-  const requesterName = body.resource.buyer_name || '고객';
-  const requesterCellphone = body.resource.buyer_cellphone || '01000000000';
-  const payDate = body.resource.payment_date.slice(0, 10);
-  const productName = body.resource.ordering_product_name || '누락';
-  const reason = body.resource.claim_reason || '미기재';
-  const orderUrl = `https://oneroommake.cafe24.com/admin/php/shop1/s_new/order_detail.php?order_id=${body.resource.order_id}&menu_no=78&bIsPinpointSearch=undefined`;
-  let info_msg = '';
+    const body = JSON.parse(event.body);
+    const today = new Date();
+    const formattedDate =
+      today.getFullYear() +
+      '-' +
+      String(today.getMonth() + 1).padStart(2, '0') +
+      '-' +
+      String(today.getDate()).padStart(2, '0');
+    const requesterName = body.resource.buyer_name || '고객';
+    const requesterCellphone = body.resource.buyer_cellphone || '01000000000';
+    const payDate = body.resource.payment_date.slice(0, 10);
+    const productName = body.resource.ordering_product_name || '누락';
+    const reason = body.resource.claim_reason || '미기재';
+    const orderUrl = `https://oneroommake.cafe24.com/admin/php/shop1/s_new/order_detail.php?order_id=${body.resource.order_id}&menu_no=78&bIsPinpointSearch=undefined`;
+    let info_msg = '';
 
-  // 고유한 요청 ID 생성
-  const keyBase = `${body.resource.order_id}-${body.resource.event_code}`;
-  const uniqueKey = crypto.createHash('md5').update(keyBase).digest('hex');
+    // 고유한 요청 ID 생성
+    const keyBase = `${body.resource.order_id}-${body.resource.event_code}`;
+    const uniqueKey = crypto.createHash('md5').update(keyBase).digest('hex');
 
-  // 중복 체크
-  if (getCache(uniqueKey)) {
-    console.log('중복된 요청입니다.');
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Duplicate webhook. Skipped.' }),
-    };
-  }
+    // 중복 체크
+    if (getCache(uniqueKey)) {
+      console.log('중복된 요청입니다.');
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Duplicate webhook. Skipped.' }),
+      };
+    }
 
-  // 보낼 메시지 구성
-  if (
-    body.resource.event_code !== 'return_order_process' &&
-    body.resource.event_code !== 'exchange_order_process'
-  ) {
-    return {
-      statusCode: 400,
-      body: 'Unhandled event type',
-    };
-  }
+    // 보낼 메시지 구성
+    if (
+      body.resource.event_code !== 'return_order_process' &&
+      body.resource.event_code !== 'exchange_order_process'
+    ) {
+      return {
+        statusCode: 400,
+        body: 'Unhandled event type',
+      };
+    }
 
-  const eventText =
-    body.resource.event_code === 'return_order_process' ? '반품' : '교환';
-  const registCate =
-    body.resource.event_code === 'return_order_process'
-      ? '반품_변심'
-      : '교환_불량';
+    const eventText =
+      body.resource.event_code === 'return_order_process' ? '반품' : '교환';
+    const registCate =
+      body.resource.event_code === 'return_order_process'
+        ? '반품_변심'
+        : '교환_불량';
 
-  const slackMessage = {
-    text: `*${eventText}이 신청 되었어요!*
+    const slackMessage = {
+      text: `*${eventText}이 신청 되었어요!*
   
   *요청날짜*: ${formattedDate}  
   *주문일자*: ${payDate}  
   *품목별 주문번호*
   ${body.resource.extra_info
-    .map((info) => `\t- ${info.ord_item_code} (${info.supplier_code})`)
+    .map((info) => {
+      const supplierName =
+        supplierMap[info.supplier_code] || info.supplier_code;
+      return `\t- ${info.ord_item_code} (${supplierName})`;
+    })
     .join('\n')}
   *주문자명(수령자명)* : ${requesterName}
   *연락처* : ${requesterCellphone}
@@ -72,145 +78,150 @@ exports.handler = async (event, context) => {
   *주문서URL*: ${orderUrl}
   *raw_data* : ${JSON.stringify(body)}
   `,
-    mrkdwn: true,
-  };
+      mrkdwn: true,
+    };
 
-  await axios.post(slackWebhookUrl, slackMessage);
+    await axios.post(slackWebhookUrl, slackMessage);
 
-  for (const info of body.resource.extra_info) {
-    const itemKey = `${body.resource.order_id}-${info.ord_item_code}-${body.event_no}`;
+    for (const info of body.resource.extra_info) {
+      const itemKey = `${body.resource.order_id}-${info.ord_item_code}-${body.event_no}`;
 
-    if (getCache(itemKey)) {
-      console.log(`중복된 품목 요청: ${itemKey}`);
-      continue;
-    }
+      if (getCache(itemKey)) {
+        console.log(`중복된 품목 요청: ${itemKey}`);
+        continue;
+      }
+      const supplierName =
+        supplierMap[info.supplier_code] || info.supplier_code;
 
-    const itemMessage = `*${eventText} 요청 - 단품 기준 티켓*
+      const itemMessage = `*${eventText} 요청 - 단품 기준 티켓*
   
   *주문일자*: ${payDate}
   *주문자명*: ${requesterName}
   *연락처*: ${requesterCellphone}
   *품목 주문번호*: ${info.ord_item_code}
-  *공급사코드*: ${info.supplier_code}
+  *공급사명*: ${supplierName}
   *상품명*: ${productName}
   *사유*: ${reason}
   *주문서URL*: ${orderUrl}
   `;
 
-    const zendeskPayload = {
-      ticket: {
-        subject: `[${eventText}] 신규 티켓! 접수 내용 확인 필요`,
-        comment: { body: itemMessage },
-        requester: {
-          name: '이자영',
-          email: 'jenny@floc.kr',
+      const zendeskPayload = {
+        ticket: {
+          subject: `[${eventText}] 신규 티켓! 접수 내용 확인 필요`,
+          comment: { body: itemMessage },
+          requester: {
+            name: '이자영',
+            email: 'jenny@floc.kr',
+          },
+          custom_fields: [
+            { id: 9316369427087, value: requesterName }, // 주문자명
+            { id: 9315295471247, value: '높음' }, // 우선순위
+            { id: 9316413678223, value: formattedDate }, // 요청날짜
+            { id: 9316416018063, value: orderUrl }, // 주문서 URL
+            { id: 9316388042767, value: `${info.ord_item_code}` }, // 품목
+            { id: 9316414895503, value: requesterCellphone },
+            { id: 9316400270479, value: registCate }, // 반품/교환 유형
+            { id: 9316386931215, value: productName }, // 상품명
+          ],
         },
-        custom_fields: [
-          { id: 9316369427087, value: requesterName }, // 주문자명
-          { id: 9315295471247, value: '높음' }, // 우선순위
-          { id: 9316413678223, value: formattedDate }, // 요청날짜
-          { id: 9316416018063, value: orderUrl }, // 주문서 URL
-          { id: 9316388042767, value: `${info.ord_item_code}` }, // 품목
-          { id: 9316414895503, value: requesterCellphone },
-          { id: 9316400270479, value: registCate }, // 반품/교환 유형
-          { id: 9316386931215, value: productName }, // 상품명
-        ],
-      },
+      };
+
+      try {
+        await retry(() =>
+          axios.post(
+            `https://${zendeskDomain}/api/v2/tickets.json`,
+            zendeskPayload,
+            {
+              headers: {
+                Authorization:
+                  'Basic ' +
+                  Buffer.from(`${zendeskEmail}/token:${zendeskToken}`).toString(
+                    'base64'
+                  ),
+                'Content-Type': 'application/json',
+              },
+            }
+          )
+        );
+
+        setCache(itemKey);
+      } catch (err) {
+        console.error(
+          `❌ Zendesk 생성 실패 (${info.ord_item_code}):`,
+          err.message
+        );
+      }
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Slack 메시지 전송 및 Zendesk 다건 처리 완료',
+      }),
     };
 
-    try {
-      await retry(() =>
-        axios.post(
-          `https://${zendeskDomain}/api/v2/tickets.json`,
-          zendeskPayload,
-          {
-            headers: {
-              Authorization:
-                'Basic ' +
-                Buffer.from(`${zendeskEmail}/token:${zendeskToken}`).toString(
-                  'base64'
-                ),
-              'Content-Type': 'application/json',
-            },
-          }
-        )
-      );
+    // try {
+    //   // Slack에 메시지 전송
+    //   await axios.post(slackWebhookUrl, message);
 
-      setCache(itemKey);
-    } catch (err) {
-      console.error(
-        `❌ Zendesk 생성 실패 (${info.ord_item_code}):`,
-        err.message
-      );
-    }
+    //   const zendeskPayload = {
+    //     ticket: {
+    //       subject: `요청 - ${body.resource.order_id}`,
+    //       comment: { body: message },
+    //       requester: {
+    //         name: 'CS/이자영',
+    //         email: 'jenny@floc.kr',
+    //       },
+    //       custom_fields: [
+    //         { id: 9316369427087, value: requesterName }, // 주문자명
+    //         { id: 9315295471247, value: '높음' }, // 우선순위
+    //         { id: 9316413678223, value: formattedDate }, // 요청날짜
+    //         { id: 9316416018063, value: orderUrl }, // 주문서URL
+    //         { id: 9316388042767, value: info_msg }, // 품목별 주문번호
+    //         { id: 9316414895503, value: requesterCellphone }, //
+    //         { id: 9316400270479, value: registCate }, // CS접수유형
+    //       ],
+    //     },
+    //   };
+
+    //   await retry(() =>
+    //     axios.post(
+    //       `https://${zendeskDomain}/api/v2/tickets.json`,
+    //       zendeskPayload,
+    //       {
+    //         headers: {
+    //           Authorization:
+    //             'Basic ' +
+    //             Buffer.from(`${zendeskEmail}/token:${zendeskToken}`).toString(
+    //               'base64'
+    //             ),
+    //           'Content-Type': 'application/json',
+    //         },
+    //       }
+    //     )
+    //   );
+
+    //   // 캐시 저장
+    //   setCache(uniqueKey);
+
+    //   return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({
+    //       message: 'Slack notification sent!',
+    //     }),
+    //   };
+    // } catch (error) {
+    //   console.error('Error sending to Slack:', error);
+    //   return {
+    //     statusCode: 500,
+    //     body: JSON.stringify({
+    //       message: 'Slack error',
+    //     }),
+    //   };
+    // }
+  } catch (e) {
+    console.log(e);
   }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Slack 메시지 전송 및 Zendesk 다건 처리 완료',
-    }),
-  };
-
-  // try {
-  //   // Slack에 메시지 전송
-  //   await axios.post(slackWebhookUrl, message);
-
-  //   const zendeskPayload = {
-  //     ticket: {
-  //       subject: `요청 - ${body.resource.order_id}`,
-  //       comment: { body: message },
-  //       requester: {
-  //         name: 'CS/이자영',
-  //         email: 'jenny@floc.kr',
-  //       },
-  //       custom_fields: [
-  //         { id: 9316369427087, value: requesterName }, // 주문자명
-  //         { id: 9315295471247, value: '높음' }, // 우선순위
-  //         { id: 9316413678223, value: formattedDate }, // 요청날짜
-  //         { id: 9316416018063, value: orderUrl }, // 주문서URL
-  //         { id: 9316388042767, value: info_msg }, // 품목별 주문번호
-  //         { id: 9316414895503, value: requesterCellphone }, //
-  //         { id: 9316400270479, value: registCate }, // CS접수유형
-  //       ],
-  //     },
-  //   };
-
-  //   await retry(() =>
-  //     axios.post(
-  //       `https://${zendeskDomain}/api/v2/tickets.json`,
-  //       zendeskPayload,
-  //       {
-  //         headers: {
-  //           Authorization:
-  //             'Basic ' +
-  //             Buffer.from(`${zendeskEmail}/token:${zendeskToken}`).toString(
-  //               'base64'
-  //             ),
-  //           'Content-Type': 'application/json',
-  //         },
-  //       }
-  //     )
-  //   );
-
-  //   // 캐시 저장
-  //   setCache(uniqueKey);
-
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify({
-  //       message: 'Slack notification sent!',
-  //     }),
-  //   };
-  // } catch (error) {
-  //   console.error('Error sending to Slack:', error);
-  //   return {
-  //     statusCode: 500,
-  //     body: JSON.stringify({
-  //       message: 'Slack error',
-  //     }),
-  //   };
-  // }
 };
 
 // retry 함수 정의
